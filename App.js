03e4b3dd5e6f5df6fc592984f068223169f611ff
taodxph09094 <taodxph09094@gmail.com>
2023-11-05 12:54:40 +0700

    .

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React, { useEffect } from 'react';
-import { Provider } from 'react-redux';
+import React, {useEffect} from 'react';
+import {Provider} from 'react-redux';
 import store from './src/redux/store';
 import {Linking, StyleSheet} from 'react-native';
 import Home from './src/Home';
@@ -32,22 +32,22 @@ const App = () => {
 
   return (
     <Provider store={store}>
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          animation: 'slide_from_bottom',
-          replaceAnimation: 'slide_from_bottom',
-        }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Question" component={Question} />
-        <Stack.Screen name="List" component={List} />
-        <Stack.Screen name="About" component={About} />
-        <Stack.Screen name="Correct" component={Correct} />
-        <Stack.Screen name="False" component={False} />
-        <Stack.Screen name="Setting" component={Setting} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+            animation: 'slide_from_bottom',
+            replaceAnimation: 'slide_from_bottom',
+          }}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Question" component={Question} />
+          <Stack.Screen name="List" component={List} />
+          <Stack.Screen name="About" component={About} />
+          <Stack.Screen name="Correct" component={Correct} />
+          <Stack.Screen name="False" component={False} />
+          <Stack.Screen name="Setting" component={Setting} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
